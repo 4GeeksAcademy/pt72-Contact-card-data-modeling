99cbda82c0a569278decee64e0aac9678d3f046c
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-11-01 22:38:02 +0000

    fin

--- src/models.py
+++ src/models.py
@@ -6,27 +6,55 @@ from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# starwars
+    # user
+    # favorites
+    # characters
+    # planets
+# instagram
+    # user
+    # followers
+    # post
+    # media
+    # comment
+    # likes
+# contact card
+    # user/agenda
+    # contact
+    # inner circle
+    
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(550), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    contacts = relationship('Contact', back_populates="user")
+    favorites = relationship('Favorites', back_populates="user")
+
+class Contact(Base):
+    __tablename__ = 'contact'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    email = Column(String(550), nullable=False)
+    address = Column(String(250))
+    phone = Column(String(250))
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
+    
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    contact_id = Column(Integer, ForeignKey('contact.id'))
+    contact = relationship(Contact)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
